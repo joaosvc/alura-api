64109b738cf1b124cb31f7c42abc3648ddcdef3a
Joaosvc <joaosvc@gmail.com>
2024-01-30 20:25:53 -0300

    feat: Adding DataNest, NestExplorer client and Nest Json Data

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { config } from "dotenv";
+import { DataNest } from "./database/data-nest";
 
 const main = async () => {
   config();
@@ -8,6 +9,8 @@ const main = async () => {
     app: express(),
     port: process.env.SERVER_PORT || 8000,
   };
+
+  await DataNest.connect();
 
   server.app.listen(server.port, () =>
     console.log(`listening on port ${server.port}!`)
