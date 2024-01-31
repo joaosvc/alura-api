d595259755fa07693406a56c2a6c860dff101dd1
Joaosvc <joaosvc@gmail.com>
2024-01-31 01:53:48 -0300

    feat: Adding ServerRouter

---
+++ src/router/server-router.ts
@@ -0,0 +1,5 @@
+import { Router } from "express";
+
+const serverRouter = Router();
+
+export default serverRouter;
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import serverRouter from "./router/server-router";
 import { config } from "dotenv";
 import { DatabaseClient } from "./database/client";
 
@@ -11,6 +12,8 @@ const main = async () => {
   };
 
   await DatabaseClient.connect();
+
+  server.app.use(serverRouter);
 
   server.app.listen(server.port, async () =>
     console.log(`listening on port ${server.port}!`)
