3fa4590f9fa4c1c45574cc40ce4163255c89030c
Joaosvc <joaosvc@gmail.com>
2024-02-01 14:03:16 -0300

    feat: Adding start.sh

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+echo -e "${GREEN}Atualizando o repositório...${NC}"
+git pull
+
+echo -e "${GREEN}Executando build...${NC}"
+npm run build
+
+echo -e "${GREEN}Iniciando a api...${NC}"
+npm run start
