e49c37f2037ff48caa040e5b60b3e95d23a53b93
Joaosvc <joaosvc@gmail.com>
2024-01-31 22:49:21 -0300

    feat: Test stream chunks

--- src/controllers/get-segment/get-segment.ts
+++ src/controllers/get-segment/get-segment.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 import express from "express";
-import { Readable } from "stream";
 import { badRequest, ok, serverError } from "../helpers";
 import { HttpRequest, HttpResponse, IController } from "../protocols";
 
@@ -9,7 +8,7 @@ export class GetVideoSegmentController implements IController {
     httpRequest: HttpRequest<unknown>,
     request: express.Request,
     response: express.Response
-  ): Promise<HttpResponse<Readable | string>> {
+  ): Promise<HttpResponse<string>> {
     try {
       const segmentId = httpRequest?.params?.id;
 
@@ -19,27 +18,48 @@ export class GetVideoSegmentController implements IController {
 
       const attachmentProxyId = decodeURIComponent(segmentId);
       const attachmentUrl = `https://cdn.discordapp.com/attachments/${attachmentProxyId}`;
-      const attachmentUuid = attachmentProxyId.split("/").pop()!;
+      // const attachmentUuid = attachmentProxyId.split("/").pop()!;
 
       const axiosResponse = await axios.get(attachmentUrl, {
         responseType: "stream",
       });
 
-      if (axiosResponse && axiosResponse.data) {
-        response.setHeader(
-          "Content-Type",
-          axiosResponse.headers["content-type"]
-        );
-        response.setHeader(
-          "Content-Disposition",
-          `attachment; filename=${attachmentUuid}`
-        );
-        //response.setHeader("Cache-Control", "public, max-age=43200");
+      const contentType = axiosResponse.headers["content-type"];
+      const contentLength = axiosResponse.headers["content-length"];
 
-        return ok<string>(axiosResponse.data);
-      } else {
-        return serverError("Failed to retrieve valid response");
-      }
+      response.setHeader("Content-Type", contentType);
+      response.setHeader("Content-Length", contentLength);
+
+      // response.setHeader(
+      //   "Content-Disposition",
+      //   `attachment; filename=${attachmentUuid}`
+      // );
+
+      // axiosResponse.data.pipe(response, { end: true });
+
+      const chunkSize = 3 * 1024 * 1024;
+
+      let offset = 0;
+      let remaining = contentLength;
+
+      const sendChunk = () => {
+        const end = Math.min(offset + chunkSize, contentLength);
+
+        if (offset < contentLength) {
+          axiosResponse.data.pipe(response, { start: offset, end });
+
+          offset += chunkSize;
+          remaining -= chunkSize;
+
+          if (remaining <= 0) {
+            response.end();
+          }
+        }
+      };
+
+      sendChunk();
+
+      return ok<string>("Streaming in progress");
     } catch (error) {
       if (error instanceof Error) {
         return badRequest(error.message);
